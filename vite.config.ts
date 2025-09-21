575bfe956961982791fdb7f6f3b0b440f7d64077
Hen Nguyen <hennv404@gmail.com>
2025-09-21 13:57:45 +0700

    feat: absoulute import path config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      'src': '/src',
+    },
+  },
 })
