eab6522502601663252b80799fbd2a76876c009c
Hen Nguyen <hennv404@gmail.com>
2025-10-24 01:40:08 +0700

    fix(hero-image): use imported image

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -2,6 +2,7 @@ import { HtmlHTMLAttributes } from "react";
 
 import Button from "src/components/Button";
 import Github from "src/components/icons/Github";
+import heroImage from "src/assets/me.jpg";
 
 function Hero(props: HtmlHTMLAttributes<HTMLDivElement>) {
   const { ...rest } = props;
@@ -12,7 +13,7 @@ function Hero(props: HtmlHTMLAttributes<HTMLDivElement>) {
       className="flex flex-col text-center gap-4 justify-start items-center bg-zinc-50 px-8 py-4 min-h-[400px]"
     >
       <div className="w-32 h-32 md:w-36 md:h-36 lg:w-80 lg:h-80 rounded-full overflow-hidden border border-zinc-900">
-        <img src="src/assets/me.jpg" className="object-cover w-full h-full" />
+        <img src={heroImage} className="object-cover w-full h-full" />
       </div>
       <p className="text-lg">
         Hello I'm <b>HEN</b>
